04d184437540b1cd7f92dcc061bf4606cb5b732a
oIITiMicT <backawork@gmail.com>
2021-03-10 19:58:01 +0300

    testing

--- main.c
+++ main.c
@@ -1,6 +1,26 @@
-#include "stdio.h"
+#include <stdio.h>
+#include <stdlib.h>
 
+struct {
+    int live;
+} elem_t;
+
+struct map{
+    int n;
+    int m;
+    struct elem_t **arr;
+} map_t;
+
+void evolve() {
+
+}
+void game() {
+
+}
 int main() {
-    printf("HELLO");
+    struct map a;
+    int n, m;
+    scanf("%d %d", &n, &m);
+    a.arr = (int**)(malloc(sizeof(int*) * n));
     return 0;
 }
