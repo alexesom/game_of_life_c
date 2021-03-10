a7d0b7d212a2ea29d004848aa44d721e7aefac22
oIITiMicT <backawork@gmail.com>
2021-03-11 00:28:30 +0300

    BREAKING CHANGE: create generator, tester, fix evolve, create alloc

---
+++ src/evolve.c
@@ -0,0 +1,5 @@
+//
+// Created by eugene on 3/11/21.
+//
+
+#include "evolve.h"
---
+++ src/evolve.h
@@ -0,0 +1,8 @@
+//
+// Created by eugene on 3/11/21.
+//
+
+#ifndef CMAKE_TESTAPP_EVOLVE_H
+#define CMAKE_TESTAPP_EVOLVE_H
+
+#endif //CMAKE_TESTAPP_EVOLVE_H
